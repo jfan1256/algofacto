82c38c7d1972f8d0f0a51693ac3b775f7ac4ce7f
jofan23 <jonathan.fan@yale.edu>
2024-01-06 20:27:14 -0500

    edited comments

--- class_strat/strat.py
+++ class_strat/strat.py
@@ -7,16 +7,18 @@ class Strategy:
         allocate (float): Percentage of capital to allocate for this strategy
         current_date (str: YYYY-MM-DD): Current date (this will be used as the end date for model training)
         threshold (int): Market cap threshold to determine whether a stock is buyable/shortable or not
+
+        Strategies will inherit the functions exec_backtest and exec_live and override it with their specific functionality
         '''
 
         self.allocate = allocate
         self.current_date = current_date
         self.threshold = threshold
 
-    # Strategies will inherit this function and override it with their specific functionality
+    # Backtest Strategy, Save Data, and Export Report
     def exec_backtest(self):
         return None
 
-    # Strategies will inherit this function and override it with their specific functionality
+    # Execute Strategy Live and Save Live Traded Stocks
     def exec_live(self):
         return None
