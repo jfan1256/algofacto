b9099976926e0b4cbcfbcbb9f2e16a8499c204ac
jofan23 <jonathan.fan@yale.edu>
2024-02-05 23:43:12 -0500

    reverted ml_train bagging

--- trade_historical/exec_strat/strat_ml_train.py
+++ trade_historical/exec_strat/strat_ml_train.py
@@ -49,7 +49,7 @@ start_time = time.time()
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------MODEL---------------------------------------------------------------------------------------------
-alpha = ModelTrain(live=live, model_name='lightgbm_trial_105', end=end, tuning='default', shap=False, plot_loss=False, plot_hist=False, pred='price', stock='permno', lookahead=1, trend=0,
+alpha = ModelTrain(live=live, model_name='lightgbm_trial_106', end=end, tuning='default', shap=False, plot_loss=False, plot_hist=False, pred='price', stock='permno', lookahead=1, trend=0,
                            incr=True, opt='wfo', weight=False, outlier=False, early=True, pretrain_len=1260, train_len=504, valid_len=63, test_len=21, **lightgbm_params)
 
 # alpha = ModelTrain(live=live, model_name='randomforest_trial_10', end=end, tuning='default', shap=False, plot_loss=False, plot_hist=False, pred='sign', stock='permno', lookahead=1,
