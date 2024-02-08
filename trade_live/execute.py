8e53f0f7538fffb32786730c7587e4e7110b832a
jofan23 <jonathan.fan@yale.edu>
2024-02-08 00:10:44 -0500

    added a reset() function

--- trade_live/execute.py
+++ trade_live/execute.py
@@ -1,5 +1,8 @@
 import asyncio
 import concurrent.futures
+import os
+import shutil
+
 import schedule
 
 from ib_insync import *
@@ -228,6 +231,55 @@ def monitor():
 
     # Monitor All Strategies
     mont_all.monitor_all()
+
+def reset():
+    '''
+    Note: Deletes all data, prices, and reports in trade_live directory's subdirectories
+
+          reset() should be executed at caution
+    '''
+
+    # Reset Logic
+    print("-"*60 + "\nReseting...")
+    for item in os.listdir(get_live_trade()):
+        item_path = os.path.join(get_live_trade(), item)
+
+        # Skip data_large reset
+        if item=='data_large':
+            continue
+
+        # Remove data in live_monitor
+        elif item=='live_monitor':
+            for item in os.listdir(item_path):
+                item_path = os.path.join(item_path, item)
+                if os.path.isdir(item_path) and item.startswith('strat_'):
+                    for sub_item in os.listdir(item_path):
+                        sub_item_path = os.path.join(item_path, sub_item)
+                        for file in os.listdir(sub_item_path):
+                            file_path = os.path.join(sub_item_path, file)
+                            if os.path.isfile(file_path) and file.endswith('.parquet.brotli'):
+                                os.remove(file_path)
+
+        # Remove data in directories that start with 'strat_'
+        elif item.startswith('strat_'):
+            for sub_item in os.listdir(item_path):
+                sub_item_path = os.path.join(item_path, sub_item)
+                # Remove data in subdirectories that are in ['report', 'result', 'data']
+                if os.path.isdir(sub_item_path) and (sub_item in ['report', 'result', 'data']):
+                    for file in os.listdir(sub_item_path):
+                        file_path = os.path.join(sub_item_path, file)
+                        if os.path.isfile(file_path):
+                            os.remove(file_path)
+                        elif os.path.isdir(file_path):
+                            shutil.rmtree(file_path)
+
+        # Remove data in directories that start with 'live_', or 'data_'
+        elif item.startswith('live_') or item.startswith('data_'):
+            for file in os.listdir(item_path):
+                file_path = os.path.join(item_path, file)
+                os.remove(file_path)
+
+    print("Finished\n" + "-"*60)
 
 # Build
 schedule.every().monday.at("00:01").do(build)
